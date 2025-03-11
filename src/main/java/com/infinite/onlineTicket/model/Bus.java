3dae7d4dcff4c240a218ec0dad8a809dc98310d5
Samikshya Timalsina <samikshyat@infinite.com>
2025-03-12 00:02:56 +0545

    feat: bus seems okay

--- src/main/java/com/infinite/onlineTicket/model/Bus.java
+++ src/main/java/com/infinite/onlineTicket/model/Bus.java
@@ -1,27 +1,35 @@
 package com.infinite.onlineTicket.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.List;
 
-//@Entity
-//@Table(name = "buses")
-public class Bus implements Serializable {
-//    @Id
-//    @SequenceGenerator(name = "buses_seq_generator", sequenceName = "buses_sequence_generator",
-//            allocationSize = 1)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "buses_sequence_generator")
-//    private Long id;
-//    @Column(name = "bus_number")
-//    private String busNumber;
-//    private Integer capacity;
-//    @ManyToMany(targetEntity = Route.class, fetch = FetchType.LAZY)
-//    @JoinTable(name ="bus_route",
-//            joinColumns= @JoinColumn(name= "bus_id", referencedColumnName = "id",
-//                    foreignKey = @ForeignKey(name = "fk_buses_bus_id")),
-//            inverseJoinColumns = @JoinColumn(name= "route_id", referencedColumnName = "id",
-//                    foreignKey = @ForeignKey(name = "fk_buses_route_id")))
-//    private List<Route> routeId;
+@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
+
+@Entity
+@Table(name = "buses")
+public class Bus implements Serializable {
+    @Id
+    @SequenceGenerator(name = "buses_seq_generator", sequenceName = "buses_sequence_generator",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "buses_sequence_generator")
+    private Long id;
+    @Column(name = "bus_number")
+    private String busNumber;
+    private Integer capacity;
+    @ManyToMany(targetEntity = Route.class, fetch = FetchType.LAZY)
+    @JoinTable(name ="bus_route",
+            joinColumns= @JoinColumn(name= "bus_id", referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "fk_buses_bus_id")),
+            inverseJoinColumns = @JoinColumn(name= "route_id", referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "fk_buses_route_id")))
+    private List<Route> routes;
 }
