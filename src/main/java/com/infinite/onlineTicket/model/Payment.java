2fed79e7de11f9f8d354b264080a232513274f50
Samikshya Timalsina <samikshyat@infinite.com>
2025-03-12 00:20:06 +0545

    fix: entities fix completed

--- src/main/java/com/infinite/onlineTicket/model/Payment.java
+++ src/main/java/com/infinite/onlineTicket/model/Payment.java
@@ -7,28 +7,28 @@ import lombok.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-//@Data
-//@Getter
-//@Setter
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
-//@Entity
-//@Table(name = "payments")
+@Entity
+@Table(name = "payments")
 public class Payment implements Serializable {
-//    @Id
-//    @SequenceGenerator(name = "payment_seq_generator", sequenceName = "payment_seq_generator",
-//            allocationSize = 1)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_seq_generator")
-//    private Long id;
-//    @Column(name = "payment_date")
-//    private LocalDateTime paymentDate;
-//    @Column(name = "payment_method")
-//    @Enumerated(EnumType.STRING)
-//    private PaymentMethod paymentMethod;
-//    @OneToOne(targetEntity = Reservation.class, fetch = FetchType.LAZY)
-//    @JoinColumn(name = "reservation_id", referencedColumnName = "id",
-//            foreignKey = @ForeignKey(name = "fk_payment_reservation_id"))
-//    private Reservation reservation;
+    @Id
+    @SequenceGenerator(name = "payment_seq_generator", sequenceName = "payment_seq_generator",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_seq_generator")
+    private Long id;
+    @Column(name = "payment_date")
+    private LocalDateTime paymentDate;
+    @Column(name = "payment_method")
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    @OneToOne(targetEntity = Reservation.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "fk_payment_reservation_id"))
+    private Reservation reservation;
 }
